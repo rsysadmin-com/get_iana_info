690e8773f6f2619439423291f74768f1e66e974a
Martin Mielke <martinm@rsysadmin.com>
2024-08-06 21:50:57 +0200

    Directly pipe contents of  to grep, thus eliminating the temp file

--- get_iana_info.sh
+++ get_iana_info.sh
@@ -2,7 +2,6 @@
 
 # Some variables
 iana_url=https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.txt
-iana_tmp_file=/var/tmp/service-names-port-numbers.txt
 stuff_to_check=$1
 
 # Usefull function
@@ -16,11 +15,5 @@ usage() {
 # Check that we have something to check, show usage otherwise
 [ -z $stuff_to_check ] && usage
 
-# Silently download $iana_tmp_file
-wget -q $iana_url -O $iana_tmp_file
-
-# Find the port or service in $iana_tmp_file
-grep $stuff_to_check $iana_tmp_file | uniq
-
-# Clean up
-rm -f $iana_tmp_file
+# Download $iana_url content silently and directly pipe to grep
+wget -qO- $iana_url | grep $stuff_to_check | uniq
